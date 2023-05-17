2c4af5c82002a3be4d829a06a6611e47eca15510
EEstaniel <edwineestaniel93@gmail.com>
2023-05-17 04:43:43 -0700

    fix: checkboxes now update state correctly

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -5,11 +5,25 @@ import {Greet, GeneratePassword} from "../wailsjs/go/main/App";
 function App() {
     const [password, setPassword] = useState("")
     const [length, setLength] = useState(12)
+    const [checkbox, setCheckbox] = useState({
+        uppercase: false,
+        lowercase: false,
+        number: false,
+        special: false
+    })
     const lengthSlider = document.getElementById("length_slider") as HTMLInputElement
     const lengthInput = document.getElementById("length_input")as HTMLInputElement;
 
 
+    function handleCheckBoxChange(e: { target: { id: string; checked: boolean; }; }) {
+        setCheckbox({
+            ...checkbox,
+            [e.target.id]: e.target.checked
+        })
+    }
+
     function generatePassword(e: { preventDefault: () => void; }) {
+        console.log(checkbox["uppercase"])
         GeneratePassword(length).then((res: any) => {
             setPassword(res)
         })
@@ -33,19 +47,19 @@ function App() {
                 </div>
                 <div className="pw-form-other">
                     <div className="checkbox">
-                        <input type="checkbox" id={"uppercase"}/>
+                        <input type="checkbox" id={"uppercase"} checked={checkbox.uppercase} onChange={handleCheckBoxChange}/>
                         <label htmlFor={"uppercase"}>uppercase</label>
                     </div>
                     <div className="checkbox">
-                        <input type="checkbox" id={"lowercase"}/>
+                        <input type="checkbox" id={"lowercase"} checked={checkbox.lowercase} onChange={handleCheckBoxChange}/>
                         <label htmlFor={"lowercase"}>lowercase</label>
                     </div>
                     <div className="checkbox">
-                        <input type="checkbox" id={"number"}/>
+                        <input type="checkbox" id={"number"} checked={checkbox.number} onChange={handleCheckBoxChange}/>
                         <label htmlFor={"number"}>number</label>
                     </div>
                     <div className="checkbox">
-                        <input type="checkbox" id={"special"}/>
+                        <input type="checkbox" id={"special"} checked={checkbox.special} onChange={handleCheckBoxChange}/>
                         <label htmlFor={"special"}>special</label>
                     </div>
                     <div className="slider">
