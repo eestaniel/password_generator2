0d13a6845d56ac27e4a8df14c8d7ac931db35da4
EEstaniel <edwineestaniel93@gmail.com>
2023-05-17 06:22:12 -0700

    fix: removed unused

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import './App.css';
-import {Greet, GeneratePassword} from "../wailsjs/go/main/App";
+import {GeneratePassword} from "../wailsjs/go/main/App";
 
 function App() {
     const [password, setPassword] = useState("")
